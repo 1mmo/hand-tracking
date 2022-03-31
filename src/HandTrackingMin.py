ab2a09edbca25e06e9a8556bafbc48d7b72dc709
yunost <yunost@yunost.localhost>
2022-03-31 12:06:16 +0300

    HandTrackingModule

--- src/HandTrackingMin.py
+++ src/HandTrackingMin.py
@@ -16,29 +16,22 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id_, lm in enumerate(handLms.landmark):
-                #print(id_, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id_, cx, cy)
                 if id_:
                     cv2.circle(img, (cx,cy), 25, (255,0,255), cv2.FILLED)
-                        
-
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
     cTime = time.time()
-    #print(cTime)
     fps = 1/(cTime-pTime)
     pTime = cTime
-
     cv2.putText(img, str(int(fps)), (10,50), cv2.FONT_HERSHEY_PLAIN, 3,
             (255,0,255), 2)
-
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
